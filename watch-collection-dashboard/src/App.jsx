08b95e183ce3ffd82e42a15abcbc7d9a47646cb8
Justin Yeo <yeojiaxing97@gmail.com>
2026-09-05 21:55:43 +0800

    minor changes

--- watch-collection-dashboard/src/App.jsx
+++ watch-collection-dashboard/src/App.jsx
@@ -12,6 +12,9 @@ function App() {
   return (
     <div className="container-fluid">
       <Navbar />
+      <br></br>
+      <h1 className="text-center">Watch Collection Dashboard</h1>
+      <br></br>
       <WatchDisplay watches={watches} />
     </div>
   )
@@ -141,12 +144,9 @@ function Watch({ watch }) {
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <nav className ="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">Chronotelier</a>
       </div>
     </nav>
   )
